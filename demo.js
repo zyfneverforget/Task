f32f7e392c6ea77d08c98e4dad0869a49d974ec0
zyfneverforget <262829233@qq.com>
2018-01-23 09:55:33 +0800

    task 5

---
+++ demo.js
@@ -0,0 +1,19 @@
+var fs = require('fs')
+
+ var dirName = process.argv[2] 
+ fs.access('dirName', (err) =>{
+     if(!err) {
+         console.err('文件已存在')
+         process.exit(1)
+     } else {
+         fs.mkdirSync("./" + dirName)
+         process.chdir("./" + dirName)
+         fs.mkdirSync('css')
+         fs.mkdirSync('js')
+         fs.writeFileSync("./index.html", "<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>")
+         fs.writeFileSync("css/style.css", "h1{color: red;}")
+         fs.writeFileSync("./js/main.js", 'var string = "Hello World"\nalert(string)')
+         process.exit(0)
+     }
+ })
+ 
